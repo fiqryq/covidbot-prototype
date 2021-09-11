c2c613bc6f268312163bc6eedb7fbf19143419aa
fiqryq <fiqrychoerudin@student.telkomuniversity.ac.id>
2021-09-11 15:26:59 +0700

    add status vaksin nasional

--- src/bot.js
+++ src/bot.js
@@ -5,7 +5,9 @@ const axios = require("axios");
 
 const bot = new Telegraf(process.env.TELEGRAM_TOKEN);
 const url = process.env.DEV_URL;
+const national_vaccine = process.env.URL_VAKSIN_NASIONAL;
 
+const { numberFormat } = require("../utils/helper");
 // on start bot
 bot.start((ctx) =>
   ctx.reply(
@@ -57,7 +59,15 @@ bot.action("menu_info", (ctx) => {
     "------------------INFO------------------",
     {
       reply_markup: {
-        inline_keyboard: [[{ text: "Info Rumah Sakit", callback_data: "rs" }]],
+        inline_keyboard: [
+          [
+            { text: "Info Rumah Sakit", callback_data: "rs" },
+            {
+              text: "Status Vaksin Nasional",
+              callback_data: "vaksin_nasional",
+            },
+          ],
+        ],
       },
     }
   );
@@ -82,6 +92,71 @@ bot.action("menu_kasus", (ctx) => {
 
 bot.action("rs", (ctx) => {
   bot.telegram.sendMessage(ctx.chat.id, "INI MENU INFO RS");
+});
+
+bot.action("vaksin_nasional", async (ctx) => {
+  try {
+    const response = await axios.get(`${national_vaccine}`);
+    const data = response.data.monitoring[0];
+    const {
+      date,
+      total_sasaran_vaksinasi,
+      sasaran_vaksinasi_sdmk,
+      sasaran_vaksinasi_petugas_publik,
+      sasaran_vaksinasi_lansia,
+      sasaran_vaksinasi_masyarakat_umum,
+      sasaran_vaksinasi_kelompok_1217,
+      vaksinasi1,
+      vaksinasi2,
+    } = data;
+    const today = moment(date).format("LL");
+    const cakupan = data.cakupan;
+    console.log(cakupan);
+
+    const message = `Data status vaksinasi nasional ${today}\n\r\n\Sasaran vaksinasi :\n\rTotal : ${numberFormat(
+      total_sasaran_vaksinasi
+    )}\n\rSDMK : ${numberFormat(
+      sasaran_vaksinasi_sdmk
+    )}\n\rPetugas Publik : ${numberFormat(
+      sasaran_vaksinasi_petugas_publik
+    )}\n\rLansia : ${numberFormat(
+      sasaran_vaksinasi_lansia
+    )}\n\rUmum : ${numberFormat(
+      sasaran_vaksinasi_masyarakat_umum
+    )}\n\rKelompok usia 12_17 : ${numberFormat(
+      sasaran_vaksinasi_kelompok_1217
+    )}\n\rDosis 1 : ${numberFormat(vaksinasi1)}\n\rDosis 2 : ${numberFormat(
+      vaksinasi2
+    )}\n\r\n\rPersentase : \n\r\n\rVaksinasi dosis 1 : ${numberFormat(
+      cakupan.vaksinasi1
+    )} \n\r-sdm kesehatan : ${numberFormat(
+      cakupan.sdm_kesehatan_vaksinasi1
+    )}\n\r-petugas publik : ${numberFormat(
+      cakupan.petugas_publik_vaksinasi1
+    )}\n\r-lansia : ${numberFormat(
+      cakupan.lansia_vaksinasi1
+    )}\n\r-umum : ${numberFormat(
+      cakupan.masyarakat_umum_vaksinasi1
+    )}\n\r-usia 12_17 : ${numberFormat(
+      cakupan.kelompok_usia_12_17_vaksinasi1
+    )}\n\r\n\rVaksinasi dosis 2 : ${numberFormat(
+      cakupan.vaksinasi2
+    )} \n\r-sdm kesehatan : ${numberFormat(
+      cakupan.sdm_kesehatan_vaksinasi2
+    )}\n\r-petugas publik : ${numberFormat(
+      cakupan.petugas_publik_vaksinasi2
+    )}\n\r-lansia : ${numberFormat(
+      cakupan.lansia_vaksinasi2
+    )}\n\r-umum : ${numberFormat(
+      cakupan.masyarakat_umum_vaksinasi2
+    )}\n\r-usia 12_17 : ${numberFormat(
+      cakupan.kelompok_usia_12_17_vaksinasi2
+    )}`;
+
+    bot.telegram.sendMessage(ctx.chat.id, message);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 bot.command("lapor", (context) => {
@@ -149,23 +224,27 @@ bot.action("today_case", async (ctx) => {
     const names = data.map((e, i) => `${i + 1}. ${e.name} #${e.faculty}`);
     const datanames = names.join("\r\n");
     const body = `Data covid ${today} : \n${datanames}`;
-    bot.telegram.sendMessage(ctx.chat.id, body);
+    if (data.length > 0) {
+      bot.telegram.sendMessage(ctx.chat.id, body);
+    } else {
+      bot.telegram.sendMessage(ctx.chat.id, "Tidak ada laporan kasus hari ini");
+    }
   } catch (error) {
     console.log(error);
   }
 });
 
-bot.action("total_case", async (ctx) => {
-  try {
-    const response = await axios.get(`${url}/totalcase`);
-    const data = response.data.data;
-    const names = data.map((e, i) => `${i + 1}. ${e.name} #${e.faculty}`);
-    const datanames = names.join("\r\n");
-    const body = `Data covid ${today} : \n${datanames}`;
-    bot.telegram.sendMessage(ctx.chat.id, body);
-  } catch (error) {
-    console.log(error);
-  }
-});
+// bot.action("total_case", async (ctx) => {
+//   try {
+//     const response = await axios.get(`${url}/totalcase`);
+//     const data = response.data.data;
+//     const names = data.map((e, i) => `${i + 1}. ${e.name} #${e.faculty}`);
+//     const datanames = names.join("\r\n");
+//     const body = `Data covid ${today} : \n${datanames}`;
+//     bot.telegram.sendMessage(ctx.chat.id, body);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// });
 
 bot.launch();
